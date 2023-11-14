9ebc3b804e38ace207f41c6ea83261454cd17571
JoEunjeong287 <amy287@naver.com>
2023-11-14 17:28:17 +0900

    14일최종

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 const MainPage = () => {
   const [headlines, setHeadlines] = useState([]);
   const [today, setToday] = useState(null);
+  const [interests, setInterests] = useState([]);
 
   useEffect(() => {
     // 가상의 헤드라인 뉴스 목록
@@ -23,30 +24,55 @@ const MainPage = () => {
       condition: 'Sunny',
     };
     setToday(dummyToday);
+
+    // 가상의 관심 컨테이너 정보
+    const dummyInterests = [
+      { id: 1, title: '관심 주제 1', description: '관심 주제 1에 대한 설명' },
+      { id: 2, title: '관심 주제 2', description: '관심 주제 2에 대한 설명' },
+      // 추가적인 관심 주제
+    ];
+    setInterests(dummyInterests);
   }, []);
 
   return (
     <div>
-      <div className="headline-container">
-        <h1>헤드라인 뉴스</h1>
-        <ul>
-          {headlines.map((headline) => (
-            <li key={headline.id}>
-              <Link to={`/news/${headline.id}`}>{headline.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="today-container">
-        <h2>오늘의 정보</h2>
-        {today && (
-          <>
-            <p>Date: {today.date}</p>
-            <p>Time: {today.time}</p>
-            <p>Temperature: {today.temperature}</p>
-            <p>Condition: {today.condition}</p>
-          </>
-        )}
+      <div className="main-container">
+        <div className="left-container">
+          <div className="headline-container">
+            <h1>헤드라인 뉴스</h1>
+            <ul>
+              {headlines.map((headline) => (
+                <li key={headline.id}>
+                  <Link to={`/news/${headline.id}`}>{headline.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="interest-container">
+            <h2>관심 컨테이너</h2>
+            <ul>
+              {interests.map((interest) => (
+                <li key={interest.id}>
+                  <h3>{interest.title}</h3>
+                  <p>{interest.description}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="right-container">
+          <div className="today-container">
+            <h2>오늘의 정보</h2>
+            {today && (
+              <>
+                <p>Date: {today.date}</p>
+                <p>Time: {today.time}</p>
+                <p>Temperature: {today.temperature}</p>
+                <p>Condition: {today.condition}</p>
+              </>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
