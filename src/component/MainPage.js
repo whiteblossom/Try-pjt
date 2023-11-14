132c76ab09ad00fc17c9453e203021d796bad24c
JoEunjeong287 <amy287@naver.com>
2023-11-14 17:06:41 +0900

    오른쪽박스만듦

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -3,30 +3,51 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const MainPage = () => {
-  // 가상의 헤드라인 뉴스 목록
   const [headlines, setHeadlines] = useState([]);
+  const [today, setToday] = useState(null);
 
   useEffect(() => {
-    // 여기서 API 호출 등을 통해 실제 데이터를 가져올 수 있습니다.
-    // 여기서는 가상의 데이터를 사용하겠습니다.
-    const dummyHeadlines = [
-      { id: 1, title: '헤드라인 뉴스 1', content: '헤드라인 뉴스 내용 1' },
-      { id: 2, title: '헤드라인 뉴스 2', content: '헤드라인 뉴스 내용 2' },
-      { id: 3, title: '헤드라인 뉴스 3', content: '헤드라인 뉴스 내용 3' },
-    ];
+    // 가상의 헤드라인 뉴스 목록
+    const dummyHeadlines = Array.from({ length: 10 }, (_, index) => ({
+      id: index + 1,
+      title: `헤드라인 뉴스 ${index + 1}`,
+      content: `헤드라인 뉴스 내용 ${index + 1}`,
+    }));
     setHeadlines(dummyHeadlines);
+
+    // 가상의 날씨 정보
+    const dummyToday = {
+      date: '2023-11-14',
+      time: '12:30 PM',
+      temperature: '25°C',
+      condition: 'Sunny',
+    };
+    setToday(dummyToday);
   }, []);
 
   return (
     <div>
-      <h1>헤드라인 뉴스</h1>
-      <ul>
-        {headlines.map((headline) => (
-          <li key={headline.id}>
-            <Link to={`/news/${headline.id}`}>{headline.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <div className="headline-container">
+        <h1>헤드라인 뉴스</h1>
+        <ul>
+          {headlines.map((headline) => (
+            <li key={headline.id}>
+              <Link to={`/news/${headline.id}`}>{headline.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="today-container">
+        <h2>오늘의 정보</h2>
+        {today && (
+          <>
+            <p>Date: {today.date}</p>
+            <p>Time: {today.time}</p>
+            <p>Temperature: {today.temperature}</p>
+            <p>Condition: {today.condition}</p>
+          </>
+        )}
+      </div>
     </div>
   );
 };
