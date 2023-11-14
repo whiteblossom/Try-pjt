6f12fa14297805e626015e76624b883aa8713529
whiteblossom <39724731+whiteblossom@users.noreply.github.com>
2023-11-14 09:46:41 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
