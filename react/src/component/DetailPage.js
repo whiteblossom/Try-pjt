ac1e9e9b69d051b9810997a90bf2cd29dc01c2b7
whiteblossom <39724731+whiteblossom@users.noreply.github.com>
2023-11-20 16:38:58 +0900

    버그덩어리

--- react/src/component/DetailPage.js
+++ react/src/component/DetailPage.js
@@ -6,16 +6,18 @@ import 'slick-carousel/slick/slick-theme.css';
 
 const DetailPage = () => {
   const { article_id } = useParams();
-  const [news, setDetailNews] = useState([]);
+  const [news, setDetailNews] = useState({ articles: [], content: '' });
   const [likes, setLikes] = useState(0);
   const [dislikes, setDislikes] = useState(0);
-
+  let NewData ; 
   useEffect(() => {
     const fetchDetailNews = async () => {
       try {
         const response = await fetch(`/api/articles/detail/${article_id}`);
         const data = await response.json();
-        setDetailNews(data);
+        NewData = { ...news , data } ;
+        setDetailNews(NewData);
+        console.log(NewData);
       } catch (error) {
         console.error('Error fetching detail news:', error.message);
       }
@@ -23,6 +25,7 @@ const DetailPage = () => {
 
     fetchDetailNews();
   }, [article_id]);
+
   if (!news) {
     return <div>Loading...</div>;
   }
@@ -34,6 +37,7 @@ const DetailPage = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+
   // 우측 상단 헤드라인 데이터
   const rightHeadlines = [
     { id: 1, title: '우측 헤드라인 1' },
@@ -45,24 +49,29 @@ const DetailPage = () => {
 
   const likeImageUrl = process.env.PUBLIC_URL + '/img/like.png';
   const dislikeImageUrl = process.env.PUBLIC_URL + '/img/dislike.png';
-  
+
   return (
     <div className="main-container">
       <div className="left-container">
-        <h1 id="title">{news.title}</h1>
-        {console.log(news.article_id)}
-        {console.log(news.title)}
-        <small>{news.journalist}</small>
-        <br />
-        <small>{news.date}</small>
-        <br />
-        <small>조회수: {news.views}</small>
-
-        <p>{news.content}</p>
+        {news.articles && news.articles.map((article) => (
+          <li key={article.id}>
+            <h1 id="title">{article.data.title}</h1>
+            <small>{article.journalist}</small>
+            <br />
+            <small>{article.data.date}</small>
+            <br />
+            <small>조회수: {article.views}</small>
+            <p>{article.content}</p>
+          </li>
+        ))}
         {/* 좋아요와 싫어요 버튼 */}
         <div className="standard">
-            <button className="like" onClick={() => setLikes(likes + 1)}><img src={likeImageUrl} alt="추천"/><br/>{likes}</button>
-            <button className="dislike" onClick={() => setDislikes(dislikes + 1)}><img src={dislikeImageUrl} alt="비추천"/><br/>{dislikes}</button>
+          <button className="like" onClick={() => setLikes(likes + 1)}>
+            <img src={likeImageUrl} alt="추천" /><br />{likes}
+          </button>
+          <button className="dislike" onClick={() => setDislikes(dislikes + 1)}>
+            <img src={dislikeImageUrl} alt="비추천" /><br />{dislikes}
+          </button>
         </div>
         <div className="interest-container">
           {/* 슬라이더 */}
@@ -85,7 +94,7 @@ const DetailPage = () => {
       </div>
       <div className="right-container">
         <h2>우측 헤드라인</h2>
-        <ul style={{padding:"0px"}}>
+        <ul style={{ padding: "0px" }}>
           {rightHeadlines.map((headline) => (
             <p key={headline.id}>{headline.title}</p>
           ))}
