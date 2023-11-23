75fc4d0cac9e7457baa88a12f15a46573f976346
JoEunjeong287 <amy287@naver.com>
2023-11-23 11:43:14 +0900

    .21

--- spring boot/TryProject/src/main/java/org/example/controller/ReadingController.java
+++ spring boot/TryProject/src/main/java/org/example/controller/ReadingController.java
@@ -1,15 +1,7 @@
 package org.example.controller;
 
 import org.example.mapper.ReadingMapper;
-<<<<<<< Updated upstream
 import org.springframework.beans.factory.annotation.Autowired;
-=======
-import org.example.model.Recommendation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.example.model.LogData;
-import org.springframework.web.bind.annotation.PathVariable;
->>>>>>> Stashed changes
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
