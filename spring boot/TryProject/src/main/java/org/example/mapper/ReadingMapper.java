f7e142e68eac20c6310cdb75759e4fdd028fe323
whiteblossom <39724731+whiteblossom@users.noreply.github.com>
2023-11-23 11:34:14 +0900

    되냐

--- spring boot/TryProject/src/main/java/org/example/mapper/ReadingMapper.java
+++ spring boot/TryProject/src/main/java/org/example/mapper/ReadingMapper.java
@@ -1,12 +1,18 @@
 package org.example.mapper;
 
 import org.apache.ibatis.annotations.*;
+import org.example.model.Recommendation;
 import org.example.model.LogData;
 
 import java.time.LocalDateTime;
 
 @Mapper
 public interface ReadingMapper {
+    @Select("SELECT recommendation FROM logdata WHERE article_id = #{article_id} AND user_id = #{user_id}")
+    Integer findRecommendation(@Param("article_id") Integer article_id, @Param("user_id") String password);
+
+    @Update("UPDATE logdata SET recommendation = #{recommendation.recommendation} WHERE user_id = #{recommendation.user_id} and article_id =#{recommendation.article_id}")
+    void handleLikeDislike(@Param("recommendation") Recommendation recommendation);
 
     @Select("SELECT * FROM logdata WHERE user_id = #{user_id} AND article_id = #{article_id}")
     LogData getLogData(@Param("user_id") String user_id, @Param("article_id") int article_id);
