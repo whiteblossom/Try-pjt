b61b1135cad25f3753bf568ff11b3e888e9e047d
JoEunjeong287 <amy287@naver.com>
2023-11-29 16:19:19 +0900

    chart4

--- react/src/component/ChartComponent.js
+++ react/src/component/ChartComponent.js
@@ -1,4 +1,3 @@
-// ChartComponent.jsx
 
 import React, { useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
@@ -6,15 +5,11 @@ import Chart from 'chart.js/auto';
 const ChartComponent = ({ data, chartType }) => {
   const chartRef = useRef(null);
 
-  // 데이터가 비어 있는 경우 0으로 초기화
-  if (!data.values) {
-    data.values = Array(data.labels.length).fill(0);
-  }
+  const maxValue = Math.max(...data.values);
 
   useEffect(() => {
     const ctx = chartRef.current.getContext('2d');
 
-    // 차트 생성
     const chart =
       chartType === 'bar'
         ? new Chart(ctx, {
@@ -36,9 +31,9 @@ const ChartComponent = ({ data, chartType }) => {
                 y: {
                   beginAtZero: true,
                   min: 0,
-                  max: 10,
+                  max:  maxValue + 2,
                   grid: {
-                    display: false, // y-축 격자 표시 여부
+                    display: false,
                   },
                 },
               },
@@ -58,7 +53,6 @@ const ChartComponent = ({ data, chartType }) => {
               ],
             },
           });
-
     return () => {
       // 컴포넌트 언마운트 시 차트 인스턴스 제거
       chart.destroy();
